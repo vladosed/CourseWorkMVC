afd98f27ab9342864c5722251d589231ed082bb2
Vladiko <v.sodolevskiy@gmail.com>
2021-06-05 21:11:00 +0300

    1.1 some errors fixed

--- PracticeWork/PracticeMVC/PracticeMVC/Models/Categoty.cs
+++ PracticeWork/PracticeMVC/PracticeMVC/Models/Categoty.cs
@@ -3,9 +3,9 @@ namespace PracticeMVC.Models
 {
     public class Categoty
     {
-        public object MyProperty { get; set; }
-        public object MyProperty { get; set; }
-        public object MyProperty { get; set; }
+        public object id { get; set; }
+        //public object MyProperty { get; set; }
+        //public object MyProperty { get; set; }
 
         
     }
